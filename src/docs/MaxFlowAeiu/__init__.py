78701ee73e4fa9d3c89783bb699038636c5925c0
sancas96 <ita.sancas@gmail.com>
2022-06-02 15:16:18 -0500

    agregando cambios docu

--- src/docs/MaxFlowAeiu/__init__.py
+++
@@ -1,1 +0,0 @@
-from .MaxFlowAeiu import MaxFlowAeiu
--- src/setup.py
+++ src/setup.py
@@ -1,10 +1,11 @@
 #see: https://setuptools.readthedocs.io/en/latest/userguide/package_discovery.html
-from setuptools import setup, Extension, find_packages
+from setuptools import setup, find_packages
+
 setup(name="MaxFlowAeiu",
-      version="0.1.4",
+      version="0.1.2",
       description=u"Execution of the Fork Fulkerson method to find the maximum flow of a network",
       long_description=open("README.md", "r", encoding="utf-8").read(),
-      url="https://github.com/optimizacion-2-2022-gh-classroom/practica-2-segunda-parte-EddOselotl",
+      url="https://github.com/optimizacion-2-2022-gh-classroom/practica-2-primera-parte-urieluard",
       author="Team-2",
       author_email="",
       classifiers = ['License :: OSI Approved :: MIT License',
@@ -14,9 +15,4 @@ setup(name="MaxFlowAeiu",
       license="MIT",
       keywords='max flow',
       packages=find_packages(),
-      ext_modules = [Extension("MaxFlowAeiu", ["MaxFlowAeiu/MaxFlowAeiu.c"])],
-      install_requires=[
-        'numpy>=1.21.5'
-        ],
-      python_requires='>=3.8'
       )
