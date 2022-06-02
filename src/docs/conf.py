0301d4b77de700d3a73adf7bbaab0ce36ce6cc78
sancas96 <ita.sancas@gmail.com>
2022-06-02 09:23:24 -0500

    agregando documento final

--- src/docs/conf.py
+++ src/docs/conf.py
@@ -24,9 +24,9 @@ copyright = '2022, Equipo2 Optimizacion2'
 author = 'Equipo2'
 
 # The short X.Y version
-version = '0.1.3'
+version = '0.1.4'
 # The full version, including alpha/beta/rc tags
-release = '0.1.3'
+release = '0.1.4'
 
 
 # -- General configuration ---------------------------------------------------
